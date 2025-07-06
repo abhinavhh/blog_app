22923a26dac059fb0e80df96f2f02f11ad2c5407
Adarsh <adarshkannanvfc@gmail.com>
2025-07-06 17:26:16 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Databse Connection
 
 const connectDB = require('./config/database');
 const app = express();
 connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type','application/json'],
+}))
 
 const port = 4000;
 app.listen(port, () => {
