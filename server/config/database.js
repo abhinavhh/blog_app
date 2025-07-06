3ac2ae4806c7b70003ce0143b10c0c8e2cee6c3f
Adarsh <adarshkannanvfc@gmail.com>
2025-07-06 15:52:42 +0530

    removed everything from server.js and added only database connection

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,40 @@
-import { connect } from 'mongoose';
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 function connectDB() {
-    connect(process.env.MONGODB_URI, {
-        useNewUriParser: true,
-        useUnifiedTopology: true,
-    }).then(() => console.log('Mongodb connected'))
-    .catch((err) => console.log('Error connecting', err));
+    try{
+        mongoose.connect(process.env.MONGODB_URI, {
+            maxPoolSize: 10,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+            bufferCommands: false,
+            retryWrites: true,
+            retryReads: true,
+            heartbeatFrequencyMS: 10000,
+            minPoolSize: 1,
+            maxIdleTimeMS: 30000,
+            connectTimeoutMS: 10000,
+            family: 4,
+        });
+        console.log('Mongodb conneted');
+
+        mongoose.connection.on('conneted', () => {
+            console.log('Mongoose connected to mongoDb');
+        });
+
+        mongoose.connection.on('error', (err) => {
+            console.error('Mongoose connection error', err);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('Mongoose disconnected from Mongodb');
+        });
+
+
+    }
+    catch(error){
+        console.log('Error connecting to MongoDb: ', error);
+    }
 }
-export default connectDB;
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,57 +1,14 @@
-// imports and setup
-
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
-const rateLimit = require('express-rate-limit');
-const cookieParser = require('cookie-parser');
 
-// Database connection
+// Databse Connection
 
 const connectDB = require('./config/database');
 const app = express();
 connectDB();
 
-// Basic Middleware
-
-app.use(express.json());
-app.use(cookieParser());
-
-// Security Enhancement
-
-app.use(helmet());
-app.disable('x-powered-by');
-
-// Request Throtting ( limit each ip to 100 requests per 15 minute)
-
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    standardHeaders: true,
-    legacyHeaders: false,
+const port = 4000;
+app.listen(port, () => {
+    console.log('server running on port 4000');
 });
-app.use(limiter);
-
-// response optimization
-
-app.use(compression());
-
-// cors config
-
-const isProd = process.env.NODE_ENV === 'production';
-app.use(cors({
-    origin: isProd ?
-        'https://front-end-url'
-        :'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type','Authorization'],
-}));
-app.options('*', cors());
-
-// trust proxy
-
-if (isProd) app.set('trust-proxy', 1);
