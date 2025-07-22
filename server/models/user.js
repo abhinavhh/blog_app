d9901bbded15a276527d872d0238bbd36a1d40d3
Abhinav <abhinavponnu964531@gmail.com>
2025-07-22 17:05:42 +0530

    implemented user schema with password hashing and role based acces

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,67 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
+// imnpor bcrypt for password hashing and comparison
+import bcrypt from 'bcrypt';
+
+// Define the User schema
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
+    username: {
+        unique: true,
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 3
+    },
+    email: {
+        unique: true,
+        type: String,
+        required: true,
+        trim: true,
+        match: /.+\@.+\..+/,
+        Lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        select: false,
+        trim: true,
+    },
+    role: {
+        trpe: String,
+        enum: ['User', 'Admin'],
+        default: 'User'
+    }
+}, { timestamps: true });
+
+// --Security : -- Store password in hashed format
+userSchema.pre('save', async function() {
+    // hash password if it has been modified or is new
+    if(!this.isModified('password')) {
+        return next();
+    }
+
+    try{
+        // create a salt and hash the password
+        const salt = await bcrypt.genSalt(10); // 10 is the salt rounds
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    }
+    catch (err) {
+        console.error('Error hashing password: ', err)
+    }
 })
 
-module.exports = mongoose.model('user', userSchema);
+// --Secuirity : -- Compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    }
+    catch (err) {
+        console.error('Error comparing password: ', err);
+        return false;
+    }
+}
+
+const User = mongoose.model('User', userSchema);
+export default User;
