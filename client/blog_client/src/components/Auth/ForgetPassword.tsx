054875df5f53a25e244d9b901575770f36295cba
Abhinav <abhinavponnu964531@gmail.com>
2025-08-08 20:30:44 +0530

    created reusable css tokens

---
+++ client/blog_client/src/components/Auth/ForgetPassword.tsx
@@ -0,0 +1,14 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+const ForgetPassword = () => {
+  return (
+    <motion.div
+        className=''
+    >
+        
+    </motion.div>
+  )
+}
+
+export default ForgetPassword
